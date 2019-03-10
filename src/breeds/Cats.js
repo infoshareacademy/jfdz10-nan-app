0a2445629da3ebc369e3c7fc002910e4288213b2
Magdalena Skopczynska <madstar@interia.pl>
2019-03-10 20:21:11 +0100

    stylin images

--- src/breeds/Cats.js
+++ src/breeds/Cats.js
@@ -1,41 +1,49 @@
 import React, { Component, Fragment } from "react";
-import './Cats.css';
-import { Image } from 'semantic-ui-react'
+import "./Cats.css";
+
 
 class Cats extends Component {
   state = {
     breeds: []
-  }
+  };
 
-  componentDidMount(){
-    fetch('/breeds.json')
+  componentDidMount() {
+    fetch("/breeds.json")
       .then(response => response.json())
       .then(data => {
-          this.setState({
-            breeds: data
-          })
-      })
+        this.setState({
+          breeds: data
+        });
+      });
   }
 
-    render() {
-     return  (
-       <Fragment>
-          <h1>Breeds of Cats</h1>
-          <div className="cat_boxes" >
-          {
-            this.state.breeds.map(el => {
-              return <div className="cats" key={el.id}>
-              <img className="images" alt={el.breed} src={el.image}/>
-              <p className="cat_img" background='${url({el.image})}'></p>
-              <p className="cat_names">{el.breed}</p>
+  render() {
+    document.body.style.background = "#ff0  ";
+    return (
+      <Fragment>
+        <h1 className="title">Breeds of Cats</h1>
+        <div className="cat_boxes">
+          {this.state.breeds.map(el => {
+            console.log(el.image)
+            return (
+              <div className="cats" key={el.id}>
+          
+                <p
+                  className="cat_img"
+                  style={
+                    { 
+                      backgroundImage: `url(${el.image})`
+                    }
+                  }
+                />
+                <p className="cat_names">{el.breed}</p>
               </div>
-            })
-          }
-          </div>
+            );
+          })}
+        </div>
       </Fragment>
-  )
-}
+    );
+  }
 }
 
-export default Cats
-
+export default Cats;
