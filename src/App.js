c884818246c48b29ebbc6b1fe849b2f5749c8f71
krolik2 <krolikmat@gmail.com>
2019-03-10 21:19:10 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-// import Cats from './breeds/Cats';
-import Breeders from './breeders/Breeders';
+import React, { Component } from "react";
+import 'semantic-ui-css/semantic.min.css'
+import { Grid, Segment} from 'semantic-ui-react';
+import "./App.css";
+import Breeders from './breeders/Breeders'
 
 class App extends Component {
   render() {
     return (
-
-        <Breeders />
-     
+      <Grid container columns={2} stretched>
+        <Grid.Column width={3}>
+        </Grid.Column>
+        <Grid.Column width={13}>
+            <Breeders />
+        </Grid.Column>
+      </Grid>
     );
   }
 }
-
-export default App
+export default App;
--- src/breeders/Breeders.js
+++ src/breeders/Breeders.js
@@ -1,35 +1,45 @@
 import React, { Component, Fragment } from "react";
+import { Card, Icon, Image, Segment, Grid } from "semantic-ui-react";
 
 class Breeders extends Component {
   state = {
     breeders: []
-  }
+  };
 
   componentDidMount() {
-        fetch('/breeders.json')
-        .then(response => response.json())
-        .then(data => {
-            this.setState({
-                breeders: data
-            })
-        })
-    }
+    fetch("/breeders.json")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          breeders: data
+        });
+      });
+  }
 
-    render() {
-     return  (
-            <Fragment>
-                <h1>Lista Hodowców</h1>
-                {
-                    this.state.breeders.map(el => {
-                        return <div>{el.name}
-                        <p>{el.img}</p>
-                        <p>{el.description}</p>
-                        </div>
-                    })
-                }
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1>Lista Hodowców</h1>
+        <Grid.Row >
+          {this.state.breeders.map(el => {
+            return (
+              <Card>
+                <Image src={el.img} />
+                <Card.Content>
+                  <Card.Header>{el.name}</Card.Header>
+                  <Card.Meta>
+                    <span className="date">Joined in 2015</span>
+                  </Card.Meta>
+                  <Card.Description>{el.description}</Card.Description>
+                </Card.Content>
+                <Card.Content extra />
+              </Card>
+            );
+          })}
+        </Grid.Row>
+      </>
+    );
+  }
 }
 
-export default Breeders
+export default Breeders;
