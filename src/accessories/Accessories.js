5c9fd96c73f059e3b878184233bd49c387308500
bbakiewicz <bbakiewicz@gmail.com>
2019-03-14 21:05:10 +0100

    products display. Need to be styled

--- src/accessories/Accessories.js
+++ src/accessories/Accessories.js
@@ -1,35 +1,43 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import { Grid, Button, Image } from "semantic-ui-react";
 
 class Accessories extends Component {
-    
-    state = {
-        accessories: []
-      }
-    
-             
-    componentDidMount(){
-        fetch('/feed-and-accessories.json')
-          .then(response => response.json())
-          .then(data => {
-              this.setState({
-                accessories: data
-              })
-          })
-    }
-    
-    render() {
-         return  (
-           <React.Fragment>
-              <h1>Hello Accessories</h1>
-              {
-                this.state.accessories.map(el => {
-                  return <div>{el.name}</div>
-                })
-              }
-          </React.Fragment>
-        )
-    }
+  state = {
+    accessories: []
+  };
+
+  componentDidMount() {
+    fetch("/feed-and-accessories.json")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          accessories: data
+        });
+      });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Accessories</h1>
+        {this.state.accessories.map(el => {
+          return (
+            <div>
+              <Grid columns="three" divided>
+                <Grid.Row>
+                  <Grid.Column>
+                    {el.name}
+                    <Image src={el.img} />
+                    <Button size="small" content="Zobacz" />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </div>
+          );
+        })}
+      </React.Fragment>
+    );
+  }
 }
 
 export default Accessories;
-
