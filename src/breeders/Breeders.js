b4c8417c26e271ae2f4bbcc14bf63dfa485f79bb
Mateusz Krolikowski <krolikmat@gmail.com>
2019-03-21 00:58:40 +0100

    code cleanup

--- src/breeders/Breeders.js
+++ src/breeders/Breeders.js
@@ -1,14 +1,8 @@
 import React, { Component } from "react";
-import {
-  Card,
-  Segment,
-  Input,
-  Button
-} from "semantic-ui-react";
+import { Card, Segment, Input, Button } from "semantic-ui-react";
+import styled from "styled-components";
 
-import styled from 'styled-components'
-
-import './Breeders.css'
+import "./Breeders.css";
 
 const StyledHeader = styled.div`
   display: flex;
@@ -18,13 +12,13 @@ const StyledHeader = styled.div`
 
 const StyledCardImage = styled.img`
   height: 200px !important;
-  max-height: 100%;  
-  max-width: 100%; 
-  width: auto;   
+  max-height: 100%;
+  max-width: 100%;
+  width: auto;
   top: 0;
   bottom: 0;
-  left: 0; 
-  right: 0; 
+  left: 0;
+  right: 0;
   margin: auto;
 `;
 
@@ -47,26 +41,32 @@ class Breeders extends Component {
     return (
       <>
         <StyledHeader>
-        <h1>Lista Hodowców</h1>
-        <Input className="input" action='Search' placeholder='Szukaj...'/>
+          <h1>Lista Hodowców</h1>
+          <Input className="input" action="Search" placeholder="Szukaj..." />
         </StyledHeader>
-        <Segment style={{margin: "0 2rem"}}>
+        <Segment style={{ margin: "0 2rem" }}>
           <Card.Group itemsPerRow={4}>
             {this.state.breeders.map(el => {
               return (
-                <Card centered >
+                <Card centered>
                   <StyledCardImage src={el.img} />
                   <Card.Content>
                     <Card.Header>{el.name}</Card.Header>
                     <Card.Description>{el.description}</Card.Description>
                   </Card.Content>
                   <Card.Content extra>
-                  <Button className="button"
-                    color='brown'
-                    content='Like'
-                    icon='heart'
-                    label={{ basic: true, color: 'brown', pointing: 'left', content: '2,048' }}
-                  />
+                    <Button
+                      className="button"
+                      color="brown"
+                      content="Like"
+                      icon="heart"
+                      label={{
+                        basic: true,
+                        color: "brown",
+                        pointing: "left",
+                        content: "2,048"
+                      }}
+                    />
                   </Card.Content>
                 </Card>
               );
@@ -77,8 +77,5 @@ class Breeders extends Component {
     );
   }
 }
-
-//TODO wyrownac zawartosc w kartach
-//TODO zmmniejszyc wysokosc segmentu z search/add 
 
 export default Breeders;
