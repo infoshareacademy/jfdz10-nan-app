7769ce1b4077f848bce74a835c9e88c35b61cfe7
Mateusz Krolikowski <krolikmat@gmail.com>
2019-03-21 20:06:39 +0100

    changed searchbar, header and like button alignment, images are responsive now

--- src/breeders/Breeders.js
+++ src/breeders/Breeders.js
@@ -6,12 +6,14 @@ import "./Breeders.css";
 
 const StyledHeader = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: 1rem 2rem 0;
 `;
 
 const StyledCardImage = styled.img`
-  height: 200px !important;
+  height: 17vw;
   max-height: 100%;
   max-width: 100%;
   width: auto;
@@ -42,7 +44,7 @@ class Breeders extends Component {
       <>
         <StyledHeader>
           <h1>Lista Hodowców</h1>
-          <Input className="input" action="Search" placeholder="Szukaj..." />
+          <Input className="input" action="Szukaj" placeholder="Szukaj..." />
         </StyledHeader>
         <Segment style={{ margin: "0 2rem" }}>
           <Card.Group itemsPerRow={4}>
@@ -54,7 +56,7 @@ class Breeders extends Component {
                     <Card.Header>{el.name}</Card.Header>
                     <Card.Description>{el.description}</Card.Description>
                   </Card.Content>
-                  <Card.Content extra>
+                  <Card.Content extra textAlign="center">
                     <Button
                       className="button"
                       color="brown"
