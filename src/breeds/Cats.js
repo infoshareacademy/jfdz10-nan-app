4eee8aceb9e48ad23ce39af2d83671925d46d031
Magda Skopczynska <madstar@interia.pl>
2019-03-22 21:23:43 +0100

    deleted grid, added segment and card.group in cat js, few changes in css style

--- src/breeds/Cats.js
+++ src/breeds/Cats.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Card, Image, Grid, Button, Input } from "semantic-ui-react";
+import { Card, Image, Button, Input, Segment } from "semantic-ui-react";
 import styled from "styled-components";
 import "./Cats.css";
 import {Route, Link} from 'react-router-dom';
@@ -8,7 +8,7 @@ import SingleCat from './SingleCat';
 const StyledHeader = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-around;
   margin: 1rem 2rem 0;
 `;
 
@@ -31,15 +31,14 @@ class Cats extends Component {
     return (
       <Fragment>
         <StyledHeader>
-        <h1 className="cat_title">Rasy kotów</h1>
-        <Input className="cat_input" action='Szukaj' placeholder='Szukaj...' />
+          <h1 className="cat_title">Rasy kotów</h1>
+          <Input className="cat_input" action='Szukaj' placeholder='Szukaj...' />
         </StyledHeader>
-        <Grid columns={2} >
-        <Grid.Row className="cat_row">
+        <Segment className="cat_segment">
+          <Card.Group className="cat_card_group" itemsPerRow={2}>
           {this.state.breeds.map(el => {
             return (
-              <Grid.Column className="cat_column" key={el.id}>
-                <Card className="cat_card">
+                <Card centered className="cat_card" key={el.id}>
                   <Image src={el.image} className="cat_img" alt="kot"/>
                   <Card.Content>             
                     <Link className="cat_name" to={`̣/cats/${el.breed}`}>{el.breed}</Link>
@@ -51,11 +50,10 @@ class Cats extends Component {
                     label={{ basic: true, color: 'brown', pointing: 'left', content: '2,048' }}
                   />
                 </Card>
-                </Grid.Column>
             );
           })}
-        </Grid.Row>
-        </Grid>
+           </Card.Group>
+        </Segment>
         <Route path="/cats/:breed" component={SingleCat} />
       </Fragment>
     );
