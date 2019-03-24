4257db271d4fd3908d42d857eb4c4954af026e69
Owca <37577436+Owczza@users.noreply.github.com>
2019-03-24 20:44:44 +0100

    Beginning of user profile

--- src/content/Content.js
+++ src/content/Content.js
@@ -6,17 +6,19 @@ import Accessories from '../accessories/Accessories'
 import SingleAccessory from '../accessories/SingleAccessory'
 import Breeders from '../breeders/Breeders'
 import Dashboard from '../dashboard/Dashboard'
+import Profile from '../user/Profile'
 
 class Content extends Component {
   render() {
     return (
 
         <div style={{height: "100vh", width: "82.25vw", position: "relative", left: "18.75vw" }}>
-        <Route key="bleh" path="/cats" component={Cats} />
-        <Route key="bleh2" exact path="/food-and-accessories" component={Accessories} />
-        <Route key="bleh3" path="/breeders" component={Breeders} />
+        <Route exact path="/cats" component={Cats} />
+        <Route exact path="/food-and-accessories" component={Accessories} />
+        <Route exact path="/breeders" component={Breeders} />
         <Route path={"/food-and-accessories/:id"} component={SingleAccessory} />
-        <Route path={"/dashboard"} component={Dashboard} />
+        <Route exact path={"/dashboard"} component={Dashboard} />
+        <Route exact path={"/profile"} component={Profile} />
         </div>
 
     );
