f6dadcb69987be52ca27ccf37cbc4a8cd3f5ea6b
bbakiewicz <bbakiewicz@gmail.com>
2019-04-02 20:58:44 +0200

    filtering by category

--- src/accessories/AccessoriesFilters.js
+++ src/accessories/AccessoriesFilters.js
@@ -1,9 +1,12 @@
 import React, { Component, Fragment } from "react";
+import { Input, Dropdown } from "semantic-ui-react";
+
+import "./Accessories.css";
 
 class AccessoriesFilters extends Component {
   state = {
-    text: '',
-    category: ''
+    text: "",
+    category: ""
   };
 
   onInputChange = e => {
@@ -12,22 +15,48 @@ class AccessoriesFilters extends Component {
     );
   };
 
-  onCategorySelect = e => {
-    this.setState(
-      {category: e.target.value},
-      () => this.props.onFilterChange(this.state)
-    )
+  onCategorySelect = (event, {value}) => {
+    this.setState({ category: value }, () =>
+      this.props.onFilterChange(this.state)
+    );
+  };
+
+  getCategoryFilter() {
+    return this.props.categories.map((category, i) => ({
+      key: i,
+      text: category,
+      value: category
+    }));
   }
 
+  
+
   render() {
+    // const { category } = this.state;
+    console.log(this.getCategoryFilter());
     return (
+      
       <Fragment>
-        <select onChange={this.onCategorySelect}>
-            <option value=''>wybierz kategorię</option>\
-            {this.props.categories.map(category => (
-                <option value={category}>{category}</option>
-            ))}
-        </select>
+          <Dropdown
+            onChange={this.onCategorySelect}
+            options={this.getCategoryFilter()}
+            selection
+            placeholder="wybierz kategorię..."
+          />
+        {/* <select onChange={this.onCategorySelect}>
+          <option value="">wybierz kategorię</option>
+          {this.props.categories.map(category => (
+            <option value={category}>{category}</option>
+          ))}
+        </select> */}
+        <Input
+          icon="search"
+          className="cat_input"
+          placeholder="wpisz czego szukasz..."
+          type="text"
+          value={this.state.value}
+          onChange={this.onInputChange}
+        />
       </Fragment>
     );
   }
