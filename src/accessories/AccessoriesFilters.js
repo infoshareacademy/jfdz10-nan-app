3a9266b4ec80508616e61fcd8848961aa34c605a
bbakiewicz <bbakiewicz@gmail.com>
2019-04-07 19:47:52 +0200

    added filtering by categories

--- src/accessories/AccessoriesFilters.js
+++ src/accessories/AccessoriesFilters.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Input, Dropdown } from "semantic-ui-react";
+import { Input, Dropdown, Button } from "semantic-ui-react";
 
 import "./Accessories.css";
 
@@ -40,17 +40,18 @@ class AccessoriesFilters extends Component {
           placeholder="wybierz kategorię..."
         />
         {/* <select onChange={this.onCategorySelect}>
-          <option value="">wybierz kategorię</option>
+          <option value="">wybierz kategorię</option>git 
           {this.props.categories.map(category => (
             <option value={category}>{category}</option>
           ))}
         </select> */}
+        <Button content='Sortuj...' icon='up arrow' labelPosition='right' />
         <Input
           icon="search"
           className="cat_input"
           placeholder="wpisz czego szukasz..."
           type="text"
-          value={this.state.value}
+          value={this.state.value} 
           onChange={this.onInputChange}
         />
       </Fragment>
