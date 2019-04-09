52fc87049ec93d9ee4c4c581485c6d194524cc64
Magda Skopczynska <madstar@interia.pl>
2019-04-09 22:43:38 +0200

    margin changed in styledheader

--- src/sharedcomponents/StyledHeader.js
+++ src/sharedcomponents/StyledHeader.js
@@ -4,12 +4,12 @@ const StyledHeader = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  margin: 2rem 2rem 1rem;
+  margin: 2rem auto;
   height: 60px;
 `;
 
 const StyledSingleTitle = styled.div`
-  margin: 2rem 2rem 1rem;
+  margin: 2rem auto;
   height: 60px;
   padding-top: 16px;
 `;
